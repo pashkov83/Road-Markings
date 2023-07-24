1a0c62b3d6682ffa8462629f87343a0197268417
Evgeniy Pashkov <pashkov.krm@ukr.net>
2023-07-24 17:18:18 +0300

    redesign

--- script.js
+++ script.js
@@ -18,50 +18,94 @@
 
 
 
-const paint = 1570
-const solvent = paint * 0.05
-const reflectiveSpheres = 250
-const proportion11 = 1
-const proportion12 = 1
-const proportion13 = 2
-const proportion14 = 1
-const proportion15 = 0.33333
-const proportion16 = 0.66666
-const proportion17 = 0.5
-const proportion18 = 0.66666
-const proportion111 = 1.66666
+const paint = 1570 //942 г на м2 при толщине 0,6 мм
+const solvent = paint * 0.05 //for plastic = plastic * 0.04
+const reflectiveSpheres = 250 //for plastic = 300
+const plastic = 5967 //г на м2 при толщине 3 мм
+const hardener = plastic * 0.02
 const transfer = 0.001
 let depthLine = 0.6
 let widthLine = 0.10
 let lengthLine = 1
 
+//единая формула==================================
+function result(math1, math2) {
+  let mult
+  mult = (math1 * lengthLine * widthLine * math2 * transfer).toFixed(4).toString().replace('.', ',')
+  return mult
+}
 
-//Заполнение ячеек значениями 
-//===========================
-function returnCellsValues() {
+//Заполнение ячеек значениями для краски
+//======================================
+function returnCellsValuesPaint() {
   const lineProportion = [1, 1, 2, 1, 0.3333, 0.6666, 0.5, 0.6666, 1.6666]
   const paintNameCells = document.getElementsByName('paint')
   const solventNameCells = document.getElementsByName('solvent')
   const reflectiveSpheresNameCells = document.getElementsByName('reflectiveSpheres')
+  const widthLineNameCells = document.getElementsByName('widthLine')
   for (i = 0; i < paintNameCells.length; i++) {
-    paintNameCells[i].textContent = (lineProportion[i] * paint * lengthLine * widthLine * depthLine * transfer).toFixed(4).toString().replace('.', ',')
-    solventNameCells[i].textContent = (lineProportion[i] * solvent * depthLine * widthLine * transfer).toFixed(4).toString().replace('.', ',')
-    reflectiveSpheresNameCells[i].textContent = (lineProportion[i] * reflectiveSpheres * widthLine * transfer).toFixed(4).toString().replace('.', ',')
+    if (i !== 2 && i !== 7) {
+      paintNameCells[i].textContent = (lineProportion[i] * paint * lengthLine * widthLine * depthLine * transfer).toFixed(4).toString().replace('.', ',')
+      solventNameCells[i].textContent = (lineProportion[i] * solvent * depthLine * widthLine * transfer).toFixed(5).toString().replace('.', ',')
+      reflectiveSpheresNameCells[i].textContent = (lineProportion[i] * reflectiveSpheres * widthLine * transfer).toFixed(4).toString().replace('.', ',')
+      widthLineNameCells[i].textContent = (widthLine * 100).toString() + ' см'
+    } else{
+      paintNameCells[i].textContent = (lineProportion[i] * paint * lengthLine * 0.1 * depthLine * transfer).toFixed(4).toString().replace('.', ',')
+      solventNameCells[i].textContent = (lineProportion[i] * solvent * depthLine * 0.1 * transfer).toFixed(5).toString().replace('.', ',')
+      reflectiveSpheresNameCells[i].textContent = (lineProportion[i] * reflectiveSpheres * 0.1 * transfer).toFixed(4).toString().replace('.', ',')
+      widthLineNameCells[2].textContent = '15 см'
+      widthLineNameCells[7].textContent = '20 см'
+    }
   }
 }
-returnCellsValues()
+
+returnCellsValuesPaint()
+
+//Заполнение ячеек значениями для пластика
+//========================================
+function returnCellsValuesPlastic() {
+  const lineProportion = [1, 1, 2, 1, 0.3333, 0.6666, 0.5, 0.6666, 1.6666]
+  const paintNameCells = document.getElementsByName('plastic')
+  const hardenerNameCells = document.getElementsByName('hardener')
+  const solventNameCells = document.getElementsByName('solvent')
+  const reflectiveSpheresNameCells = document.getElementsByName('reflectiveSpheres')
+  const widthLineNameCells = document.getElementsByName('widthLine')
+  for (i = 0; i < paintNameCells.length; i++) {
+    if (i !== 2 && i !== 7) {
+      paintNameCells[i].textContent = (lineProportion[i] * plastic * lengthLine * widthLine * transfer).toFixed(4).toString().replace('.', ',')
+      hardenerNameCells[i].textContent = (lineProportion[i] * hardener * lengthLine * widthLine * transfer).toFixed(5).toString().replace('.', ',')
+      solventNameCells[i].textContent = (lineProportion[i] * plastic * 0.04 * widthLine * transfer).toFixed(5).toString().replace('.', ',')
+      reflectiveSpheresNameCells[i].textContent = (lineProportion[i] * (reflectiveSpheres + 50) * widthLine * transfer).toFixed(4).toString().replace('.', ',')
+      widthLineNameCells[i].textContent = (widthLine * 100).toString() + ' см'
+    } else {
+      paintNameCells[i].textContent = (lineProportion[i] * plastic * lengthLine * 0.1 * transfer).toFixed(4).toString().replace('.', ',')
+      hardenerNameCells[i].textContent = (lineProportion[i] * hardener * lengthLine * 0.1 * transfer).toFixed(5).toString().replace('.', ',')
+      solventNameCells[i].textContent = (lineProportion[i] * plastic * 0.04 * 0.1 * transfer).toFixed(5).toString().replace('.', ',')
+      reflectiveSpheresNameCells[i].textContent = (lineProportion[i] * (reflectiveSpheres + 50) * 0.1 * transfer).toFixed(4).toString().replace('.', ',')
+      widthLineNameCells[2].textContent = '15 см'
+      widthLineNameCells[7].textContent = '20 см'
+    }
+  }
+}
+returnCellsValuesPlastic()
 
 //Изменение ширины линии (type=radio)
 //==================================
 function radioWidthLine() {
   if (document.getElementById('flexRadioDefault2').checked === true) {
     widthLine = 0.15,
-      returnCellsValues()
+      returnCellsValuesPaint(),
+      returnCellsValuesPlastic()
   } else {
     widthLine = 0.10,
-      returnCellsValues()
+      returnCellsValuesPaint(),
+      returnCellsValuesPlastic()
   }
 }
+
+
+
+
 
 
 
